2111c70195d47faa99cd63348272704838275643
Luke Aslanian <laslanian@gpo.gov>
2025-02-07 15:46:34 -0500

    add package.sw3ift for build \

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "ClarityDictation",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
+    products: [
+        .executable(
+            name: "ClarityDictation",
+            targets: ["ClarityDictation"])
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "ClarityDictation",
+            dependencies: [],
+            path: "Sources/ClarityDictation"),
+        .testTarget(
+            name: "ClarityDictationTests",
+            dependencies: ["ClarityDictation"])
+    ]
+)
