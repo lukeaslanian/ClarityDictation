682438d97dde8b3edfe0c228ec80e04e229f25ae
Luke Aslanian <laslanian@gpo.gov>
2025-02-07 15:57:34 -0500

    Remove Info.plist processing from ClarityDictation package resources in Package.swift

--- ClarityDictation/ClarityDictationApp.swift
+++ ClarityDictation/ClarityDictationApp.swift
@@ -2,6 +2,25 @@ import SwiftUI
 
 @main
 struct ClarityDictationApp: App {
+    init() {
+        // Set up default UserDefaults values
+        let defaults: [String: Any] = [
+            "vibrationEnabled": true,
+            "audioFocusEnabled": true,
+            "inputLanguagesString": "en",
+            "currentInputLanguagePos": 0,
+            "overlayCharacters": "()-:!?,.",
+            "instantOutput": false,
+            "outputSpeed": 5,
+            "stylePromptSelection": 1,
+            "stylePromptCustomText": "",
+            "resendButton": false,
+            "instantRecording": false,
+            "postProcessingEnabled": true
+        ]
+        UserDefaults.standard.register(defaults: defaults)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
